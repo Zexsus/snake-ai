3d3ec6ce817b46845e353b900a7300d02f27d037
jakub <dziewonski.j@gmail.com>
2018-11-30 16:15:42 +0100

    Cleanup

--- app/app.js
+++ app/app.js
@@ -4,28 +4,27 @@ const directions = require('./modules/Directions.js');
 
 let game = new Game();
 
-initUserInput(game.snake);
+initUserInput(game);
 
-function initUserInput(snake){
+/**
+ *
+ * @param {Game} game
+ */
+function initUserInput(game){
     document.onkeydown = function(e){
         e = e || window.event;
         if(e.keyCode == '38'){
             // up arrow
-
-            snake.setDirection(directions.up);
-
+            game.snake.setDirection(directions.up);
         }else if(e.keyCode == '40'){
             // down arrow
-            snake.setDirection(directions.down);
-
+            game.snake.setDirection(directions.down);
         }else if(e.keyCode == '37'){
             // left arrow
-            snake.setDirection(directions.left);
-
+            game.snake.setDirection(directions.left);
         }else if(e.keyCode == '39'){
             // right arrow
-            snake.setDirection(directions.right);
-
+            game.snake.setDirection(directions.right);
         }
     }
 }

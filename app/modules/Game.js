90f61082093c488f1e75f578291cb0920e0f09cf
jakub <dziewonski.j@gmail.com>
2018-11-30 08:09:54 +0100

    Snake movement displaying

--- app/modules/Game.js
+++ app/modules/Game.js
@@ -7,11 +7,15 @@ const Snake = require('./Snake.js');
 
 class Game {
     constructor(){
+        this.isRunning = false;
        if(!this.gameAwaken) this.awake();
        this.start();
     }
 
     awake(){
+        this.engine = null;
+        this.grid = null;
+
         this.engine = new Engine({
             document: document,
             fps: 30,
@@ -31,6 +35,7 @@ class Game {
                 }
             }
         });
+
         this.grid = new Grid(this.engine, 32, 32);
         this.grid.setupWalls().clear();
         this.engine.centeralize(this.grid, this.engine.canvas);
@@ -43,32 +48,35 @@ class Game {
 
     start(){
         this.snake = this.initSnake();
+        this.isRunning = true;
         this.counter = 0;
 
         this.engine.update(() => {
-            this.update();
+            if(this.isRunning)
+                this.update();
         });
+
     }
 
-    over(){
-        debugger;
-        reset();
-    }
-
-    restart(){
-        initFunction();
-        delete this.snake;
-        grid.clear();
-    }
 
     initSnake(){
         let snake = new Snake();
         snake.head.setPosition({x: 10, y: 10});
+        snake.grow().grow().grow();
         snake.setDirection(directions.right);
         return snake;
     }
 
-
+    over(){
+        this.isRunning = false;
+        this.restart();
+    }
+    restart(){
+        this.snake = this.initSnake();
+        this.grid.clear();
+        this.grid.draw();
+        this.isRunning = true;
+    }
 
     update(){
         if(this.counter >= this.engine.loop.fps / 3){
@@ -79,19 +87,47 @@ class Game {
     }
 
     updatePerFrame(){
+        this.grid.resetStatesWithoutWall();
+        this.grid.clear();
         this.snake.move();
         this.handleCollisions(this.snake, this.grid, this.engine);
-        let headRect = this.grid.getItem(this.snake.head.position.y, this.snake.head.position.x);
-        headRect.state = this.engine.getState('head');
-        this.grid.clear();
+        let snakeBodyArrayForGrid = this.getSnakeBodyArrayForGrid(this.snake);
+        this.grid.setMultipleItemsState(snakeBodyArrayForGrid.reverse());
         this.grid.draw();
+    }
+
+    getSnakeBodyArrayForGrid(snake) {
+        let snakeBody = snake.getFullBody();
+        let preparedGridSnake = [
+            {
+                position: {
+                    x: snake.head.position.x,
+                    y: snake.head.position.y
+                },
+                state: this.engine.getState('head')
+            }
+        ];
+
+
+        snakeBody.forEach((value, index) => {
+            if(index === 0) return;
+            preparedGridSnake.push(
+                {
+                    position: {
+                        x: value.position.x,
+                        y: value.position.y
+                    },
+                    state: this.engine.getState('body')
+                }
+            )
+        });
+       return preparedGridSnake;
     }
 
     handleCollisions(){
         let item = this.grid.getItem(this.snake.head.position.y, this.snake.head.position.x);
-        console.log(item.state, item.position, this.snake.head.position,item.position == this.snake.head.position );
         if(item.state === this.engine.getState('wall')){
-            onWallCollision();
+            this.onWallCollision();
         }
     }
 
